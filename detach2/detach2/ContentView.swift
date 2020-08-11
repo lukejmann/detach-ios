dc2b7e2c432012868cd2ba1487b23bad780b15e9
Luke Mann <luke@Lukes-MBP.lan>
2020-08-11 15:51:48 -0400

    first

---
+++ detach2/detach2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  detach2
+//
+//  Created by Luke Mann on 8/11/20.
+//  Copyright © 2020 Luke Mann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
