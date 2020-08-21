21f9200fdd5f0429c19db08e87ad7ec0406eb5e9
Luke Mann <luke@Lukes-MBP.lan>
2020-08-21 00:23:55 -0400

    aye

--- detach2/detach2/upgrade/UpgradeScreen.swift
+++ detach2/detach2/upgrade/UpgradeScreen.swift
@@ -21,11 +21,18 @@ struct UpgradeScreen: View {
                         self.setScreen("HomeMenu")
                     }
                     Text("Upgrade To Detach Plus for $0.99 monthly.").font(.custom("Georgia-Italic", size: 25)).padding(.top, 30)
-                    BulletRow(text: "Create Unlimited Sessions").padding(.top, 72)
-                    BulletRow(text: "Support Regular App Updates\nAnd Improvements").padding(.top, 42)
-                    BulletRow(text: "Daily DNS Proxy Filter Updates ").padding(.top, 42)
+                    BulletPoints()
+                    HStack(alignment: .center) {
+                        ZStack(alignment: .center) {
+                            Rectangle().frame(width: 243, height: 57).foregroundColor(self.colorScheme == .dark ? Color.white : Color.black)
+                            Text("PURCHASE DETACH PLUS").font(.system(size: 16, weight: .regular, design: .default)).foregroundColor(self.colorScheme == .dark ? Color.black : Color.white)
+                        }
+                    }.padding(.top, 80).frame(maxWidth: .infinity)
+                    Text("TERMS AND CONDITIONS").font(.system(size: 14, weight: .regular, design: .default)).foregroundColor(self.colorScheme == .dark ? Color.white : Color.black).padding(.top,58)
+                    Text("RESTORE PURCHASE").font(.system(size: 14, weight: .regular, design: .default)).foregroundColor(self.colorScheme == .dark ? Color.white : Color.black).padding(.top,6)
 
-//                    Text("SELECT WHICH APPS ARE BLOCKED DURING\nA SESSON").font(.system(size: 14, weight: .regular, design: .default)).padding(.top, 10)
+
+  
                 }.padding(.horizontal, 37)
 
             }.padding(.top, 80).frame(width: geo.size.width, height: geo.size.height, alignment: .topLeading)
@@ -34,29 +41,21 @@ struct UpgradeScreen: View {
 }
 
 struct Bullet: View {
-    var text: String
-
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
 //        GeometryReader { geo in
 
-            HStack {
-                ZStack(alignment: .center) {
-                    Rectangle()
-                        .border(self.colorScheme == .dark ? Color.white : Color.black, width: 2)
-                        .frame(width: 20, height: 20, alignment: .leading)
-                        .foregroundColor(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
-                    Rectangle()
-                        .frame(width: 8, height: 8, alignment: .leading).foregroundColor(Color(hue: 0, saturation: 0, brightness: self.colorScheme == .dark ? 1 : 0, opacity: 1.0))
-                }
-                Text(self.text).multilineTextAlignment(.leading).lineLimit(nil).font(.system(size: 20, weight: .regular, design: .default)).frame(minWidth: 0,
-                        maxWidth: .infinity,
-                        minHeight: 0,
-                        maxHeight: .infinity,
-                        alignment: .topLeading
-                )
+        HStack {
+            ZStack(alignment: .center) {
+                Rectangle()
+                    .border(self.colorScheme == .dark ? Color.white : Color.black, width: 2)
+                    .frame(width: 20, height: 20, alignment: .leading)
+                    .foregroundColor(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
+                Rectangle()
+                    .frame(width: 8, height: 8, alignment: .leading).foregroundColor(Color(hue: 0, saturation: 0, brightness: self.colorScheme == .dark ? 1 : 0, opacity: 1.0))
             }
+        }
 //        }
     }
 }
@@ -66,7 +65,30 @@ struct UpgradeScreen_Previews: PreviewProvider {
         UpgradeScreen {
             _ in
         }
-        .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
-        .previewDisplayName("iPhone 11 Pro")
+        .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        .previewDisplayName("iPhone 11")
+    }
+}
+
+struct BulletPoints: View {
+    var body: some View {
+        Group {
+            HStack {
+                Bullet()
+                Text("Create Unlimited Sessions").padding(.leading, 6).multilineTextAlignment(.leading).lineLimit(nil).font(.system(size: 20, weight: .regular, design: .default))
+            }.padding(.top, 72)
+            HStack {
+                Bullet()
+                VStack(alignment: .leading) {
+                    Text("Support Regular App Updates").padding(.leading, 6).multilineTextAlignment(.leading).lineLimit(nil).font(.system(size: 20, weight: .regular, design: .default))
+                    Text("and Improvements").padding(.leading, 6).multilineTextAlignment(.leading).lineLimit(nil).font(.system(size: 20, weight: .regular, design: .default))
+                }
+
+            }.padding(.top, 42)
+            HStack {
+                Bullet()
+                Text("Daily DNS Proxy Filter Updates").padding(.leading, 6).multilineTextAlignment(.leading).lineLimit(nil).font(.system(size: 20, weight: .regular, design: .default))
+            }.padding(.top, 42)
+        }
     }
 }
